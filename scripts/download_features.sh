8c21c31f237cbdd1283c2f2a62de34ec5991c966
Manuel Guenther <guenther@ifi.uzh.ch>
2024-07-25 17:38:07 +0200

    Fixed download links

--- scripts/download_features.sh
+++ scripts/download_features.sh
@@ -5,16 +5,16 @@ if [ ! -d features ]; then
 fi
 
 cd features
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E1.tar.gz&dl=1
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E2.tar.gz&dl=1
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E3.tar.gz&dl=1
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E5.tar.gz&dl=1
+wget -O RFW_E1.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E1.tar.gz&dl=1"
+wget -O RFW_E2.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E2.tar.gz&dl=1"
+wget -O RFW_E3.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E3.tar.gz&dl=1"
+wget -O RFW_E5.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FRFW_features%2FRFW_E5.tar.gz&dl=1"
 
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E1.tar.gz&dl=1
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E2.tar.gz&dl=1
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E3.tar.gz&dl=1
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E4.tar.gz&dl=1
-wget https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E5.tar.gz&dl=1
+wget -O VGG2_short_E1.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E1.tar.gz&dl=1"
+wget -O VGG2_short_E2.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E2.tar.gz&dl=1"
+wget -O VGG2_short_E3.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E3.tar.gz&dl=1"
+wget -O VGG2_short_E4.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E4.tar.gz&dl=1"
+wget -O VGG2_short_E5.tar.gz "https://seafile.ifi.uzh.ch/d/46473c78a796425a8022/files/?p=%2Fpretrained_features%2FVGG2_features%2FVGG2_short_E5.tar.gz&dl=1"
 
 
 for f in "*.tar.gz"; do
